34ae6fde038c5824bdbc9ff2b326e47e7c6c5bc2
Avinash Madasu <avinash.sai001@gmail.com>
2018-11-06 23:13:09 +0530

    RCNN in pytorch

---
+++ PyTorch/model.py
@@ -0,0 +1,37 @@
+import torch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import torch.utils.data
+
+
+class rcnn(nn.Module):
+	def __init__(self,embed_dim,lstm_hidden,dense_hidden,embed_matrix):
+		super(rcnn,self).__init__()
+		self.embed_dim = embed_dim
+		self.lstm_hidden = lstm_hidden
+		self.dense_hidden = dense_hidden
+		self.embed_matrix = embed_matrix
+		self.embed = nn.Embedding.from_pretrained(self.embed_matrix)
+		self.embed.weight.requires_grad = False
+		self.lstm = nn.LSTM(self.embed_dim,self.lstm_hidden,batch_first=True)
+		self.dense1 = nn.Linear(400,self.dense_hidden)
+		self.act1 = nn.Tanh()
+		self.dense2 = nn.Linear(self.dense_hidden,1)
+		self.act2 = nn.Sigmoid()
+
+	def forward(self,gen,left,right):
+		gen_out = self.embed(gen)
+		left_out,_ = self.lstm(self.embed(left),None)
+		right_out,_ = self.lstm(self.embed(right),None)
+
+		right_out = torch.flip(right_out,[0])
+
+		concat_out = torch.cat((gen_out,left_out,right_out),-1)
+		final_out = self.act1(self.dense1(concat_out))
+
+		final_out = torch.max(final_out,dim=1)[0]
+		final_out = self.act2(self.dense2(final_out))
+
+		return final_out
---
+++ PyTorch/train.py
@@ -0,0 +1,51 @@
+import os
+import re
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def get_optimizer(optim,model):
+	if(optim=='adam'):
+		return torch.optim.Adam(model.parameters(),lr=0.001)
+	elif(optim=='sgd'):
+		return torch.optim.sgd(model.parameters(),lr=0.01)
+	else:
+		raise ValueError("Please Enter optimizers (adam,sgd)")
+
+def get_accuracy(net,loader):
+
+	correct = 0
+	total = 0
+
+	for batchidx,(gen,left,right,labels) in enumerate(loader):
+		labels = labels.double()
+		pred = net(gen,left,right)
+		pred = pred.view(pred.size(0))
+		ypred = (pred>0.5).double()
+
+		correct+=torch.sum(ypred==labels).item()
+		total+=labels.size(0)
+
+	return ((correct/total)*100)
+
+
+def train_model(model,train_loader,test_loader,optim,epochs):
+	optimizer = get_optimizer(optim,model)
+
+	for epoch in range(epochs):
+		for batchidx,(gen,left,right,labels) in enumerate(train_loader):
+			labels = labels.view(-1,1).float()
+
+			output = model(gen,left,right)
+			loss = F.binary_cross_entropy(output,labels)
+
+			model.zero_grad()
+			loss.backward()
+			optimizer.step()
+
+		train_accuracy = get_accuracy(model,train_loader)
+		print("Epoch {} Train Loss {} Train Accuracy {}".format(epoch,loss,train_accuracy))
+
+	test_accuracy = get_accuracy(model,test_loader)
+	print("Test Accuracy {}".format(test_accuracy))
+
